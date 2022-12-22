32c4f953a691f870ab237d893d95a84442bc6e22
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-22 16:36:26 +0000

    add styles and created components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <Header title="Hello"/>
+  <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+</div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Tasks
+  },
+  data(){
+    return{
+      tasks:[]
+    }
+  },
+  methods:{
+    deleteTask(id){
+     this.tasks=this.tasks.filter(t => t.id !== id)
+    }
+  },
+  created(){
+    this.tasks=[
+      {
+        id:1,
+        text:'Appointment in Paragon',
+        day:'Dec 22 at 2:30',
+        reminder:true
+      },
+      {
+        id:2,
+        text:'Check UC account',
+        day:'Dec 22 at 4:30',
+        reminder:true
+      },
+      {
+        id:3,
+        text:'Buy some food in Aldi',
+        day:'Dec 22 at 6:10',
+        reminder:false
+      },
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
